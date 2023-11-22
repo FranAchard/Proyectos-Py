079e1fa38b09d780ca72d8cb9643ce9271ab49bc
FranAchard <franachard8@gmail.com>
2023-11-22 16:18:29 -0300

    app clima

--- App-Clima/app-clima.py
+++ App-Clima/app-clima.py
@@ -1,1 +1,13 @@
-from tkinter import *
+from tkinter import*
+
+ventana = Tk()
+ventana.title("App Clima")
+ventana.geometry("350x550")
+
+texto_ciudad = Entry(ventana, font= ("Courier", 20, "normal"), justify="center")
+texto_ciudad.pack(padx=30, pady=30)
+
+obtener_clima= Button(ventana,text="Obtener Clima")
+obtener_clima.pack()
+
+ventana.mainloop
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Proyectos-Py
+# Aplicaciones creadas con Python
